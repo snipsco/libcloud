c34bbe74c16c8315b321b4639d60c24b50bc3482
Jose Alfredo Alvarez Aldana <jose.alvarez@snips.ai>
2018-10-11 18:14:29 +0200

    Property type not supported by GCEHealthCheck, for the moment is not necessary. This was intended for debugging in ansible, but even for the moment is not being used.

--- libcloud/compute/drivers/gce.py
+++ libcloud/compute/drivers/gce.py
@@ -8369,7 +8369,7 @@ class GCENodeDriver(NodeDriver):
         extra['host'] = healthcheck.get('host')
 
         return GCEHealthCheck(
-            id=healthcheck['id'], name=healthcheck['name'], type=healthcheck['type'],
+            id=healthcheck['id'], name=healthcheck['name'],
             path=healthcheck.get('requestPath'), port=healthcheck.get('port'),
             interval=healthcheck.get('checkIntervalSec'),
             timeout=healthcheck.get('timeoutSec'),
